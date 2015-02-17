3e9619cc35e28ca998770991565ed9132b723778
Moni <Moni@equipo-moni>
2015-02-17 14:47:03 +0100

    Primer Cambio

--- src/pruebas/Pruebas.java
+++ src/pruebas/Pruebas.java
@@ -20,7 +20,7 @@ public class Pruebas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Esto es una prueba de ver si se sube lo nuevo");
     }
     private static TipoMod llegirAct(String msg) {
         TipoMod act = null;
